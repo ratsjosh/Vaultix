4508d0a161af60e51ea6a516f759a00aa505cb3f
nixholas <nixholas@outlook.com>
2017-03-08 21:34:58 +0800

    Factories: OCRFactory: Migration
    Signed-off-by: nixholas <nixholas@outlook.com>

---
+++ src/Vaultix/Factories/OCRFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vaultix.Factories
+{
+    public class OCRFactory
+    {
+        public static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+    }
+}
